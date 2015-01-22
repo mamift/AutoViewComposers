038ee7ffe1c7d051ad613899863a5c59b618cf72
Muhammad Miftah <mamift@gmail.com>
2015-01-22 16:42:48 +1000

    Added config keys for setting file extensions for views and view composers.

--- src/config/config.php
+++ src/config/config.php
@@ -4,4 +4,6 @@ return array(
     'root_namespace'            => 'Mamift\ViewComposers',
     'views_path'                => app_path() . '/views/',
     'view_composers_path'       => app_path() . '/composers/',
+    'view_extension'            => '.tpl',
+    'view_composer_extension'   => '.composer.php'
 );
