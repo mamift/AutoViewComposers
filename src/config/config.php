bdd563ea834ae644885698614183caf94c85589c
Muhammad Miftah <mamift@gmail.com>
2015-01-25 18:31:13 +1000

    Changed to 5.4 array notation.

--- src/config/config.php
+++ src/config/config.php
@@ -1,9 +1,9 @@
 <?php
 
-return array(
+return [
     'root_namespace'            => 'Mamift\ViewComposers',
     'views_path'                => app_path() . '/views/',
     'view_composers_path'       => app_path() . '/composers/',
     'view_extension'            => '.blade.php',
     'view_composer_extension'   => '.composer.php'
-);
+];
